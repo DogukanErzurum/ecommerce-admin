5965ccc875110576c848d2862ef2b9ee21814eee
Dogukanerzurum06 <93072322+Dogukanerzurum06@users.noreply.github.com>
2024-04-16 18:10:46 +0300

    prisma and mongo db connection provided. Tests of the store addition process were performed. Tests were successful.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+
 import { ModalProvider } from "@/providers/modal-provider";
+import { ToasterProvider } from "@/providers/toast-provider";
+
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +22,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <ToasterProvider />
           <ModalProvider />
           {children}
         </body>
